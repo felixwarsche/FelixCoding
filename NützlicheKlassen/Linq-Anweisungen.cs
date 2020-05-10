f586768c02c0b185c23e3a05b8ba82a6739335d4
Felix Warschewski <fwarschewski@aixitem.de>
2020-05-10 16:07:05 +0200

    init

---
+++ NützlicheKlassen/Linq-Anweisungen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NützlicheKlassen
+{
+    /// <summary>
+    /// Linq-Anweisungen, welche häufig benutzt werden.
+    /// </summary>
+    class Linq_Anweisungen
+    {
+    }
+}
